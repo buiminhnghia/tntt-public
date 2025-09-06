3844c13b57becea8f51f184974531505979ef9d9
nghiabui <27.buiminhnghia.12a7@gmail.com>
2025-09-06 10:44:17 +0700

    firts

--- main.py
+++ main.py
@@ -70,7 +70,16 @@ def insert_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+app.route("/", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
 
+    if username == "admin" and password == "password":
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"message": "Invalid credentials"}), 401
     
     
 # Chạy local
